373032439d683bf5eab865b513fb1da4500e6baf
Natsumi.A <natsumi.a16@gmail.com>
2017-11-02 23:50:52 +0900

    6-7まで完了

--- Practice/src/practice06/PTra06_07.java
+++ Practice/src/practice06/PTra06_07.java
@@ -25,5 +25,37 @@ public class PTra06_07 {
 		 * ■□□□□
 		 */
 
+
+		for(int i = -1; i < 5; i++) {
+
+			for(int j = 0; j < i+1; j++) {
+
+				System.out.print("⬛︎︎︎");
+			}
+
+			for(int x = 5; x > i+1; x--) {
+
+				System.out.print("⬜︎");
+			}
+
+				System.out.println("");
+		}
+
+		for(int i = -1; i < 3; i++) {
+
+			for(int j = 4; j > i+1; j--) {
+
+				System.out.print("⬛︎︎︎");
+			}
+
+			for(int x = 0; x <= i + 1; x++) {
+
+				System.out.print("⬜︎");
+			}
+
+				System.out.println("");
+		}
+
+
 	}
 }
