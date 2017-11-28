514d305fa4cf81fe830537b5b32357ad62a538f6
Natsumi.A <natsumi.a16@gmail.com>
2017-11-28 16:02:55 +0900

    課題13-6まで完了

--- Practice/src/practice13/ptra13/PTra13_01.java
+++ Practice/src/practice13/ptra13/PTra13_01.java
@@ -6,7 +6,9 @@
  */
 package practice13.ptra13;
 
-public class PTra13_01 {
+import practice13.common.Animal;
+
+public class PTra13_01 extends practice13.common.Animal{
 
 	/*
 	 * ★ PTra13_01クラスはcommon.Animalクラスを継承してください
@@ -15,12 +17,15 @@ public class PTra13_01 {
 	public static void main(String[] args) {
 
 		// ★ PTra13_01をインスタンス化をしてください
-
+		Animal PTra13_01 = new Animal();
 
 		// ★ PTra13_01の持つsetterを実行してください（引数："人"）
-
+		PTra13_01.setName("人");
 
 		// ★ PTra13_01の持つインスタンスメソッドrunとbreathを実行してしてください
+
+		PTra13_01.run();
+		PTra13_01.breath();
 
 	}
 }
--- Practice/src/practice13/ptra13/PTra13_07.java
+++ Practice/src/practice13/ptra13/PTra13_07.java
@@ -6,6 +6,10 @@
  */
 package practice13.ptra13;
 
+import practice13.common.Item;
+import practice13.common.Slime;
+import practice13.common.SuperHero;
+
 public class PTra13_07 {
 
 	/*
@@ -15,13 +19,15 @@ public class PTra13_07 {
 	public static void main(String[] args) {
 
 		// ★ SuperHeroインスタンスとSlimeインスタンスを作成し、それぞれの名前に"勇者（装備あり）", "スライム"を設定してください
-
+		SuperHero superHero = new SuperHero();
+		Slime slime = new Slime();
 
 		// ★ Itemクラスのインスタンスを作成し、("こんぼう", 4）をコンストラクタの引数にしてください
+		Item item = new Item("棍棒", 4);
 
 
 		// ★ 作成したItemインスタンスをSuperHeroに持たせてください
-
+		superHero.setEquipment(item);
 
 		/*
 		 * ★ SuperHeroとSlimeを、どちらかが体力０になるまで戦わせます
