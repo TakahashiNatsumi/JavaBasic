bbd48e1e74a928d1275990f8457892b6f75d54ee
Natsumi.A <natsumi.a16@gmail.com>
2017-12-05 13:41:23 +0900

    18-2まで完了

--- Practice/src/practice18/PTra18_01.java
+++ Practice/src/practice18/PTra18_01.java
@@ -1,4 +1,5 @@
 /*
+
  * PTra18_01.java
  *   作成	LIKEIT	2017
  *------------------------------------------------------------
@@ -20,6 +21,7 @@ public class PTra18_01 {
             while (scanner.hasNext()) {
                 String line = scanner.nextLine();
                 // ★ 1行ごとにArrayListに格納してください
+                array.add(line);
 
             }
         } catch (FileNotFoundException e) {
@@ -28,6 +30,12 @@ public class PTra18_01 {
 
 		// ★ ArrayListに格納された値を全て表示してください
 		// ※ できれば拡張for文を使いましょう
+        		for(String words:array) {
+
+        			System.out.println(words);
+
+        		}
+
 
 	}
 }
--- Practice/src/practice18/PTra18_02.java
+++ Practice/src/practice18/PTra18_02.java
@@ -6,6 +6,13 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import practice18.entity.Player;
+
 public class PTra18_02 {
 
 	/*
@@ -33,8 +40,44 @@ public class PTra18_02 {
 		 */
 
 
+		ArrayList<Player> array = new ArrayList<>();
+
+		try (Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))){
+
+			while(scanner.hasNext()) {
+				Player player = new Player();
+
+				String line = scanner.nextLine();
+				String[] lineArray = line.split(",",4);
+
+				player.setCountry(lineArray[0]);
+				player.setName(lineArray[1]);
+				player.setPosition(lineArray[2]);
+				player.setTeam(lineArray[3]);
+
+				array.add(player);
+
+
+
+
+			}
+
+
+
+		}catch(IOException e){
+
+			System.out.println("例外発生");
+
+		}
+
 		// ★ ArrayListに格納されているインスタンス全てのtoStringメソッドを実行し、出力してください
 		// ※ できれば拡張for文を使いましょう
+
+		for(Player i: array) {
+
+			System.out.println(i.toString());
+
+		}
 
 	}
 }
